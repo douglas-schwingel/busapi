94731bb1fd1d2aeac19ff8c033bfd8e2e3da204d
Douglas Schwingel <dgls.sch@gmail.com>
2019-08-02 12:02:27 -0300

    Commitando de novo pq esqueci de tirar o import

--- src/main/java/br/com/busapi/BusapiApplication.java
+++ src/main/java/br/com/busapi/BusapiApplication.java
@@ -2,7 +2,6 @@ package br.com.busapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 public class BusapiApplication {
