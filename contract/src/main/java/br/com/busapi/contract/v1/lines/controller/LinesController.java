c6f76aceb88f09da3145980b3a25d46f7ba1ffa4
Douglas Schwingel <dgls.sch@gmail.com>
2019-08-08 22:07:45 -0300

    Revert "Adicionando CrossOrigin para o Angular"
    This reverts commit 59b00d71f439d148a7ec0027a2a2c6e6d08dc126.

--- contract/src/main/java/br/com/busapi/contract/v1/lines/controller/LinesController.java
+++ contract/src/main/java/br/com/busapi/contract/v1/lines/controller/LinesController.java
@@ -17,7 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+
 @Api(tags = {"Lines", "v1"}, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
         produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @RestController
